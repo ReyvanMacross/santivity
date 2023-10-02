48fbcef7d4eee247c85d142330872d6808b3a099
ReyvanMacross <88920121+ReyvanMacross@users.noreply.github.com>
2023-10-02 22:17:06 +0700

    updated aja

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,9 +16,7 @@ export default function NavBar() {
             <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8  ">
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <a href="#">
                         <img src="https://i.imgur.com/2ErERXm.png" class="h-12 mr-6" alt="Santivity Logo" />
-                        </a>
                         <div className="md:hidden">
                             <button
                                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border "

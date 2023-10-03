f96025b4fc067816f429f30d6e2211a8bc9714f7
ReyvanMacross <88920121+ReyvanMacross@users.noreply.github.com>
2023-10-03 22:31:23 +0700

    new updated

--- src/VideoComponent.js
+++ src/VideoComponent.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import './App.css'; // Add your CSS file
+import './App.css'; // Include your CSS file
 
 const VideoComponent = () => {
   return (
     <div id='video' className="bg-gray-100 py-7">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold text-center mb-4">Santivity Clip</h1>
-        <p className="text-lg text-center mb-8">This is our video company and some project</p>
-        <div className="flex flex-col md:flex-row  md:space-x-4 items-center">
-
-          <div className="w-full md:w-1/2">
-            <h2 className="text-xl font-semibold mb-1 text-center">Company Video</h2>
-            <div className="responsive-iframe-container">
+        <p className="text-lg text-center mb-8">This is our company video and some projects</p>
+        <div className="flex justify-center">
+          <div className="w-full md:w-2/3 lg:w-1/2">
+            <h2 className="text-xl font-semibold mb-3 text-center">Company Video</h2>
+            <div className="relative" style={{ paddingBottom: "56.25%" }}>
               <iframe
-                width="660"    // Set the width to 660 pixels
-                height="300"   // Set the height to 300 pixels
+                className="absolute top-0 left-0 w-full h-full"
                 src="https://www.youtube.com/embed/qQtPAggFMt8"
                 title="YouTube Video 1"
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
-            </div>
-          </div>
-
-          <div className="w-full md:w-1/2">
-            <h2 className="text-xl font-semibold mb-1 text-center">Game Video</h2>
-            <div className="responsive-iframe-container">
-              <iframe
-                width="660"    // Set the width to 660 pixels
-                height="300"   // Set the height to 300 pixels
-                src="https://www.youtube.com/embed/OV9NJGTLm-4"
-                title="YouTube Video 2"
                 frameBorder="0"
                 allowFullScreen
               ></iframe>
